41daa76eca29369b780ceeaee69bda932ad94ffc
Sui Chen <suichen@google.com>
2020-07-01 21:58:39 -0700

    clang-formatted and switching to hexdump

--- boost_handler_timeline_vis.js
+++ boost_handler_timeline_vis.js
@@ -1,4 +1,4 @@
-// This script deals with Boost handlers
+// This script deals with Boost ASIO handlers
 // Will need to add code to visualize one-shot events
 
 // Fields: HandlerNumber, Level,
@@ -7,171 +7,166 @@
 
 // Will use g_StartingSec as the starting of epoch
 
-
-const FILENAME_BOOST_HANDLER = "/usr/local/google/home/suichen/Documents/semitruck_perf/boost_asio_trace/fdeo29_20200617_psusensor_boost_handlers";
-
-var ASIO_Data      = [];
+var ASIO_Data = [];
 var ASIO_Timestamp = [];
 
 function FindFirstEntrySlot(slots) {
-	let i=0;
-	for (; i < slots.length; i++) {
-		if (slots[i] == undefined) break;
-	}
-	if (i >= slots.length) slots.push(undefined);
-	return i;
+  let i = 0;
+  for (; i < slots.length; i++) {
+    if (slots[i] == undefined) break;
+  }
+  if (i >= slots.length) slots.push(undefined);
+  return i;
 }
 
 function SimplifyDesc(desc) {
-	const idx0 = desc.indexOf("0x");
-	if (idx0 == -1) return desc;
-	else {
-		const d1 = desc.substr(idx0+2);
-		let idx1 = 0;
-		while (idx1+1 < d1.length && (
-			(d1[idx1] >= '0' && d1[idx1] <= '9') ||
-			(d1[idx1] >= 'A' && d1[idx1] <= 'F') ||
-			(d1[idx1] >= 'a' && d1[idx1] <= 'f'))) { idx1++ }
-		return desc.substr(0, idx0) + d1.substr(idx1)
-	}
+  const idx0 = desc.indexOf('0x');
+  if (idx0 == -1)
+    return desc;
+  else {
+    const d1 = desc.substr(idx0 + 2);
+    let idx1 = 0;
+    while (idx1 + 1 < d1.length &&
+           ((d1[idx1] >= '0' && d1[idx1] <= '9') ||
+            (d1[idx1] >= 'A' && d1[idx1] <= 'F') ||
+            (d1[idx1] >= 'a' && d1[idx1] <= 'f'))) {
+      idx1++;
+    }
+    return desc.substr(0, idx0) + d1.substr(idx1)
+  }
 }
 
 function ParseBoostHandlerTimeline(content) {
-	let parsed_entries = [];
-  const lines = content.split("\n");
+  let parsed_entries = [];
+  const lines = content.split('\n');
+  let slots = [];               // In-flight handlers
+  let in_flight_id2level = {};  // In-flight ID to level
 
-	let slots = [ ]; // In-flight handlers
-	let in_flight_id2level = { }; // In-flight ID to level
+  for (let lidx = 0; lidx < lines.length; lidx++) {
+    const line = lines[lidx];
+    if (line.startsWith('@asio|') == false) continue;
+    const sp = line.split('|');
 
-	for (let lidx = 0; lidx < lines.length; lidx ++) {
-		const line = lines[lidx];
-		if (line.startsWith("@asio|") == false) continue;
-    const sp = line.split("|");
-		
-		const tag = sp[0], ts = sp[1], action = sp[2], desc = sp[3];
-		let handler_id = -999;
-		let ts_sec = parseFloat(ts);
-		const simp_desc = SimplifyDesc(desc);
+    const tag = sp[0], ts = sp[1], action = sp[2], desc = sp[3];
+    let handler_id = -999;
+    let ts_sec = parseFloat(ts);
+    const simp_desc = SimplifyDesc(desc);
 
-		if (action.indexOf("*") != -1) {
-			const idx = action.indexOf("*");
-			const handler_id = parseInt(action.substr(idx+1));
+    if (action.indexOf('*') != -1) {
+      const idx = action.indexOf('*');
+      const handler_id = parseInt(action.substr(idx + 1));
       const level = FindFirstEntrySlot(slots);
 
-			// Create an entry here
-			let entry = [handler_id, level, ts_sec, undefined, undefined, desc, simp_desc, []]
+      // Create an entry here
+      let entry = [
+        handler_id, level, ts_sec, undefined, undefined, desc, simp_desc, []
+      ];
 
       slots[level] = entry;
-			in_flight_id2level[handler_id] = level;
-		} else if (action[0] == '>') { // The program enters handler number X
+      in_flight_id2level[handler_id] = level;
+    } else if (action[0] == '>') {  // The program enters handler number X
       handler_id = parseInt(action.substr(1));
       if (handler_id in in_flight_id2level) {
-				const level = in_flight_id2level[handler_id];
-				let entry = slots[level];
-				entry[3] = ts_sec;
-			}
-		} else if(action[0] == '<') {
-			handler_id = parseInt(action.substr(1));
+        const level = in_flight_id2level[handler_id];
+        let entry = slots[level];
+        entry[3] = ts_sec;
+      }
+    } else if (action[0] == '<') {
+      handler_id = parseInt(action.substr(1));
       if (handler_id in in_flight_id2level) {
-				const level = in_flight_id2level[handler_id];
-				let entry = slots[level];
-				entry[4] = ts_sec;
-				slots[level] = undefined;
-				parsed_entries.push(entry);
-				delete in_flight_id2level[handler_id];
-			}
-		} else if (action[0] == '.') { // syscalls
-		}
-	}
+        const level = in_flight_id2level[handler_id];
+        let entry = slots[level];
+        entry[4] = ts_sec;
+        slots[level] = undefined;
+        parsed_entries.push(entry);
+        delete in_flight_id2level[handler_id];
+      }
+    } else if (action[0] == '.') {  // syscalls
+    }
+  }
 
-	console.log("Boost handler log: " + parsed_entries.length + " entries" +
-	            ", " + slots.length + " levels")
-	ASIO_Data = parsed_entries
-	return parsed_entries
+  console.log(
+      'Boost handler log: ' + parsed_entries.length + ' entries' +
+      ', ' + slots.length + ' levels');
+  ASIO_Data = parsed_entries;
+  return parsed_entries;
 }
 
 function Group_ASIO(preprocessed, group_by) {
-  let grouped = { };
-	const IDXES = { "Layout Level":1, "Description":5, "Description1":6 }
-	for (var n=0; n<preprocessed.length; n++) {
-		var key = ""
-		for (var i=0; i<group_by.length; i++) {
-			if (i > 0) key += " "; key += ("" + preprocessed[n][IDXES[group_by[i]]]);
-		}
-		if (grouped[key] == undefined) grouped[key] = []
-		grouped[key].push(preprocessed[n])
-	}
-	console.log("[Group_ASIO] |grouped|=" + grouped.length)
-	return grouped
+  let grouped = {};
+  const IDXES = {'Layout Level': 1, 'Description': 5, 'Description1': 6};
+  for (var n = 0; n < preprocessed.length; n++) {
+    var key = ''
+    for (var i = 0; i < group_by.length; i++) {
+      if (i > 0) key += ' ';
+      key += ('' + preprocessed[n][IDXES[group_by[i]]]);
+    }
+    if (grouped[key] == undefined) grouped[key] = [];
+    grouped[key].push(preprocessed[n]);
+  }
+  return grouped;
 }
 
 function OnGroupByConditionChanged_ASIO() {
-  var tags = ["bah1", "bah2", "bah3"];
-	const v = boost_asio_handler_timeline_view;
-	v.GroupBy    = [];
-	v.GroupByStr = "";
-	for (let i=0; i<tags.length; i++) {
-		let cb = document.getElementById(tags[i]);
-		if (cb.checked) {
-			v.GroupBy.push(cb.value);
-			if (v.GroupByStr.length > 0) { v.GroupByStr += ", "; }
-			v.GroupByStr += cb.value;
-		}
-	}
-	let preproc = ASIO_Data;
-	let grouped = Group_ASIO(preproc, v.GroupBy);
+  var tags = ['bah1', 'bah2', 'bah3'];
+  const v = boost_asio_handler_timeline_view;
+  v.GroupBy = [];
+  v.GroupByStr = '';
+  for (let i = 0; i < tags.length; i++) {
+    let cb = document.getElementById(tags[i]);
+    if (cb.checked) {
+      v.GroupBy.push(cb.value);
+      if (v.GroupByStr.length > 0) {
+        v.GroupByStr += ', ';
+      }
+      v.GroupByStr += cb.value;
+    }
+  }
+  let preproc = ASIO_Data;
+  let grouped = Group_ASIO(preproc, v.GroupBy);
   GenerateTimeLine_ASIO(grouped);
-	boost_asio_handler_timeline_view.IsCanvasDirty = true;
+  boost_asio_handler_timeline_view.IsCanvasDirty = true;
 }
 
 function GenerateTimeLine_ASIO(grouped) {
   const keys = Object.keys(grouped);
-	let sortedKeys = keys.slice();
-	let intervals = [];
-	let titles    = [];
+  let sortedKeys = keys.slice();
+  let intervals = [];
+  let titles = [];
 
-	const was_starting_time_undefined = (g_StartingSec == undefined)
+  const was_starting_time_undefined = (g_StartingSec == undefined);
 
-	for (let i=0; i<sortedKeys.length; i++) {
-		titles.push(sortedKeys[i]);
-		line = [];
-		for (let j=0; j<grouped[sortedKeys[i]].length; j++) {
-			let entry = grouped[sortedKeys[i]][j];
-			let t0 = parseFloat(entry[3]);
-			let t1 = parseFloat(entry[4]);
+  for (let i = 0; i < sortedKeys.length; i++) {
+    titles.push(sortedKeys[i]);
+    line = [];
+    for (let j = 0; j < grouped[sortedKeys[i]].length; j++) {
+      let entry = grouped[sortedKeys[i]][j];
+      let t0 = parseFloat(entry[3]);
+      let t1 = parseFloat(entry[4]);
 
-			if (was_starting_time_undefined) {
-				if (g_StartingSec == undefined) {
-					g_StartingSec = t0;
-				}
-				g_StartingSec = Math.min(g_StartingSec, t0);
-			}
+      if (was_starting_time_undefined) {
+        if (g_StartingSec == undefined) {
+          g_StartingSec = t0;
+        }
+        g_StartingSec = Math.min(g_StartingSec, t0);
+      }
 
-			line.push([t0, t1, entry]);
-		}
-		intervals.push(line);
-	}
+      line.push([t0, t1, entry]);
+    }
+    intervals.push(line);
+  }
 
   // Time shift
-	for (let i=0; i<intervals.length; i++) {
-		for (let j=0; j<intervals[i].length; j++) {
-			let x = intervals[i][j];
-			x[0] -= g_StartingSec;
-			x[1] -= g_StartingSec;
-		}
-	}
+  for (let i = 0; i < intervals.length; i++) {
+    for (let j = 0; j < intervals[i].length; j++) {
+      let x = intervals[i][j];
+      x[0] -= g_StartingSec;
+      x[1] -= g_StartingSec;
+    }
+  }
 
-	console.log("[GenerateTimeLine_ASIO] |intervals|=" + intervals.length)
-	console.log("[GenerateTimeLine_ASIO] |titles|=" + titles.length)
-
-	boost_asio_handler_timeline_view.Intervals = intervals.slice()
-	boost_asio_handler_timeline_view.Titles    = titles.slice()
-}
-
-function TestFileRead_BoostHandler() {
-  let data = fs.readFileSync(FILENAME_BOOST_HANDLER, {encoding:"utf-8"});
-	ParseBoostHandlerTimeline(data);
-	OnGroupByConditionChanged_ASIO();
+  boost_asio_handler_timeline_view.Intervals = intervals.slice();
+  boost_asio_handler_timeline_view.Titles = titles.slice();
 }
 
 // Main view object for Boost handler timeline
@@ -183,59 +178,57 @@ function draw_timeline_boost_asio_handler(ctx) {
   boost_asio_handler_timeline_view.Render(ctx);
 }
 
-let Canvas_Asio = document.getElementById("my_canvas_boost_asio_handler")
+let Canvas_Asio = document.getElementById('my_canvas_boost_asio_handler');
 
 Canvas_Asio.onmousemove = function(event) {
-	const v = boost_asio_handler_timeline_view;
+  const v = boost_asio_handler_timeline_view;
   v.MouseState.x = event.pageX - this.offsetLeft;
   v.MouseState.y = event.pageY - this.offsetTop;
-  if (v.MouseState.pressed == true) { // Update highlighted area
+  if (v.MouseState.pressed == true) {  // Update highlighted area
     v.HighlightedRegion.t1 = v.MouseXToTimestamp(v.MouseState.x);
   }
   v.OnMouseMove();
   v.IsCanvasDirty = true;
 
-	v.linked_views.forEach(function(u) {
-		u.MouseState.x = event.pageX - Canvas_Asio.offsetLeft;
-		u.MouseState.y = 0; // Do not highlight any entry
-		if (u.MouseState.pressed == true) { // Update highlighted area
-			u.HighlightedRegion.t1 = u.MouseXToTimestamp(u.MouseState.x);
-		}
-		u.OnMouseMove();
-		u.IsCanvasDirty = true;
-	});
-}
+  v.linked_views.forEach(function(u) {
+    u.MouseState.x = event.pageX - Canvas_Asio.offsetLeft;
+    u.MouseState.y = 0;                  // Do not highlight any entry
+    if (u.MouseState.pressed == true) {  // Update highlighted area
+      u.HighlightedRegion.t1 = u.MouseXToTimestamp(u.MouseState.x);
+    }
+    u.OnMouseMove();
+    u.IsCanvasDirty = true;
+  });
+};
 
 Canvas_Asio.onmousedown = function(event) {
-	if (event.button == 0) {
-		boost_asio_handler_timeline_view.OnMouseDown();
-	}
+  if (event.button == 0) {
+    boost_asio_handler_timeline_view.OnMouseDown();
+  }
 };
 
 Canvas_Asio.onmouseup = function(event) {
   if (event.button == 0) {
-		boost_asio_handler_timeline_view.OnMouseUp();
+    boost_asio_handler_timeline_view.OnMouseUp();
   }
-}
+};
 
 Canvas_Asio.onwheel = function(event) {
-	event.preventDefault();
-	const v = boost_asio_handler_timeline_view;
-	if (v.IsMouseOverTimeline()) {
-		let dz = 0;
-		if (event.deltaY > 0) { // Scroll down, zoom out
-			dz = -0.3;
-		} else if (event.deltaY < 0) {  // Scroll up, zoom in
-			dz = 0.3;
-		}
-		v.Zoom(dz,
-			v.MouseXToTimestamp(
-				v.MouseState.x));
-	} else {
-		if (event.deltaY > 0) { 
-			v.ScrollY(1);
-		} else if (event.deltaY < 0) { 
-			v.ScrollY(-1);
-		}
-	}
+  event.preventDefault();
+  const v = boost_asio_handler_timeline_view;
+  if (v.IsMouseOverTimeline()) {
+    let dz = 0;
+    if (event.deltaY > 0) {  // Scroll down, zoom out
+      dz = -0.3;
+    } else if (event.deltaY < 0) {  // Scroll up, zoom in
+      dz = 0.3;
+    }
+    v.Zoom(dz, v.MouseXToTimestamp(v.MouseState.x));
+  } else {
+    if (event.deltaY > 0) {
+      v.ScrollY(1);
+    } else if (event.deltaY < 0) {
+      v.ScrollY(-1);
+    }
+  }
 }
--- renderer.js
+++ renderer.js
@@ -6,54 +6,54 @@
 // process.
 
 class Renderer {
-	constructor() {
-		let c1 = document.getElementById('my_canvas_ipmi');
-		let c2 = document.getElementById('my_canvas_dbus');
-		let c3 = document.getElementById('my_canvas_boost_asio_handler');
-		this.canvas1 = c1;
-		this.canvas2 = c2;
-		this.canvas3 = c3;
-		this.width1 = c1.width; this.height1 = c1.height;
-		this.width2 = c2.width; this.height2 = c2.height;
-		this.width3 = c3.width; this.height3 = c3.height;
-		this.ctx1 = this.canvas1.getContext('2d');
-		this.ctx2 = this.canvas2.getContext('2d');
-		this.ctx3 = this.canvas3.getContext('2d');
-		this.frame_count = 0;
-		this.addBindings();
-		this.addListeners();
-		this.update();
-		this.run();
-	}
+  constructor() {
+    let c1 = document.getElementById('my_canvas_ipmi');
+    let c2 = document.getElementById('my_canvas_dbus');
+    let c3 = document.getElementById('my_canvas_boost_asio_handler');
+    this.canvas1 = c1;
+    this.canvas2 = c2;
+    this.canvas3 = c3;
+    this.width1 = c1.width;
+    this.height1 = c1.height;
+    this.width2 = c2.width;
+    this.height2 = c2.height;
+    this.width3 = c3.width;
+    this.height3 = c3.height;
+    this.ctx1 = this.canvas1.getContext('2d');
+    this.ctx2 = this.canvas2.getContext('2d');
+    this.ctx3 = this.canvas3.getContext('2d');
+    this.frame_count = 0;
+    this.addBindings();
+    this.addListeners();
+    this.update();
+    this.run();
+  }
 
-	addBindings() {
-		this.update = this.update.bind(this);
-        this.run    = this.run.bind(this);
-	}
+  addBindings() {
+    this.update = this.update.bind(this);
+    this.run = this.run.bind(this);
+  }
 
-	addListeners() {
-		window.addEventListener('resize', this.update);
-	}
+  addListeners() {
+    window.addEventListener('resize', this.update);
+  }
 
-	update() {
-        console.log("update, " + 
-                    window.innerWidth + " x " + 
-                    window.innerHeight);
-        if (false) {
-            this.width1 = window.innerWidth;
-            this.height1 = window.innerHeight;
-            this.canvas1.width  = this.width1;
-            this.canvas1.height = this.height1;
-        }
-	}
+  update() {
+    console.log('update, ' + window.innerWidth + ' x ' + window.innerHeight);
+    if (false) {
+      this.width1 = window.innerWidth;
+      this.height1 = window.innerHeight;
+      this.canvas1.width = this.width1;
+      this.canvas1.height = this.height1;
+    }
+  }
 
-	run() {
-		draw_timeline(this.ctx1);
-		draw_timeline_dbus(this.ctx2);
-		draw_timeline_boost_asio_handler(this.ctx3);
-		window.requestAnimationFrame(this.run);
-	}
+  run() {
+    draw_timeline(this.ctx1);
+    draw_timeline_dbus(this.ctx2);
+    draw_timeline_boost_asio_handler(this.ctx3);
+    window.requestAnimationFrame(this.run);
+  }
 }
 
 g_renderer = new Renderer();
-
